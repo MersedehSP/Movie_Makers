447b74eb374c54bea2b306e53ee23e9958dc2b33
Joseph Lubrano <jlubranos@optimum.net>
2021-02-27 22:02:12 -0500

    2-27-2021

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import json
 import random
 import time
 import pandas as pd
+from joblib import dump, load
 from flask import Flask, render_template, request, jsonify
 # Flask Setup
 app =  Flask(__name__)
@@ -14,8 +15,17 @@ def home():
 
 @app.route('/modelResults', methods=['POST'])
 def modelResults():
-    
-    return jsonify()
+    X=[]
+    inputData = request.form.getlist("inputData[]")
+    inputData = [int(i) for i in inputData] 
+    X.append(inputData)
+    classifier = load('Final_ML_Model.joblib')
+    result=classifier.predict(X)
+    if (result==0):
+        outcome="Unsuccessful"
+    else:
+        outcome="Successful"
+    return outcome
     
 if __name__=="__main__":
     app.run(debug=True)
